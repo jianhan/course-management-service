5ff2f46b80d625d58ed1ed207e798e57c065f9a6
Jian Han <jianhan@mail.com>
2018-01-26 01:13:00 +1000

    Updated proto

--- db/session.go
+++ db/session.go
@@ -1,12 +1,11 @@
 package db
 
 import (
-	"github.com/spf13/viper"
 	mgo "gopkg.in/mgo.v2"
 )
 
 func NewMongodSession(url string) (*mgo.Session, error) {
-	s, err := mgo.Dial(viper.GetString("mongo.url"))
+	s, err := mgo.Dial(url)
 	if err != nil {
 		return nil, err
 	}
