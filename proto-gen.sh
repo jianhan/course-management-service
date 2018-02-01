07f377b6601db066283c3c5b3cdd46f5cddb5392
Jian Han <jianhan@mail.com>
2018-02-01 18:34:24 +1000

    [DEV] Updated protobuf added relationship definitions

--- proto-gen.sh
+++ proto-gen.sh
@@ -5,7 +5,6 @@ echo generating "$file"
 protoc --go_out=plugins=micro,grpc:. $file
 done
 
-
 echo start to inject protobuf
 for file in `find proto | grep -E "\.pb.go$"`; do
 echo injecting "$file"
